e956b5d0e37cb6b6a675f96fca419bd4dabc6a97
Eugene Sokolov <greatsokol@gmail.com>
2024-11-18 16:05:47 +0300

    authguard added

--- src/app/exposing-module/auth.guard.ts
+++ src/app/exposing-module/auth.guard.ts
@@ -4,7 +4,8 @@ import {AuthService} from '@@auth-lib';
 
 export const canActivate: CanActivateFn = () => {
   return new Promise((resolve, reject) => {
-    (inject(AuthService)).subscribeOnKeycloakReady(() => resolve(true));
+    const authService = (inject(AuthService));
+    return authService.isLoggedIn() ? true : authService.subscribeOnKeycloakReady(() => resolve(true));
   });
 }
 
