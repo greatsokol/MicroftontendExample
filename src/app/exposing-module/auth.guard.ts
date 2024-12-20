c9c230fcf0f7a40edc225310cf3980898476d1f8
Eugene Sokolov <greatsokol@gmail.com>
2024-12-20 13:13:06 +0300

    auth-lib fix

--- src/app/exposing-module/auth.guard.ts
+++ src/app/exposing-module/auth.guard.ts
@@ -5,17 +5,13 @@ import {AuthService} from '@@auth-lib';
 export const canActivate: CanActivateFn = () => {
   const authService = inject(AuthService);
   if (authService.isLoggedIn()) {
-    console.log("authService ready sync");
+    console.debug("authService ready sync");
     return true;
   } else return new Promise((resolve, reject) => {
-    if (authService.isLoggedIn()) {
-      return resolve(true);
-    } else {
-      authService.subscribeOnKeycloakReadyOnce(() => {
-        console.log("authService ready async");
-        resolve(true);
-      });
-    }
+    authService.onLoggedIn().subscribe(() => {
+      console.debug("authService ready async");
+      resolve(true);
+    });
   });
 }
 
